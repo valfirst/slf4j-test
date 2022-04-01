0b348a82615b68bb6cab30419b161f3549bed944
Valery Yatsynovich <valery_yatsynovich@epam.com>
2022-04-01 17:14:19 +0300

    Site checkin for project SLF4J Test

--- apidocs/package-search-index.js
+++ apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.github.valfirst.slf4jtest"},{"l":"org.slf4j.impl"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.github.valfirst.slf4jtest"},{"l":"org.slf4j.impl"},{"l":"uk.org.lidalia.lang"}]
