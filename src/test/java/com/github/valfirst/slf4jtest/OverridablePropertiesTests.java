d406bfc8e3bcecfc226fa340f074a3ac51fe5211
Valery Yatsynovich <valfirst@yandex.ru>
2023-07-17 15:31:50 +0300

    Replace `PowerMock` with pure `Mockito`

--- src/test/java/com/github/valfirst/slf4jtest/OverridablePropertiesTests.java
+++ src/test/java/com/github/valfirst/slf4jtest/OverridablePropertiesTests.java
@@ -3,46 +3,46 @@ package com.github.valfirst.slf4jtest;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.powermock.api.mockito.PowerMockito.whenNew;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mockConstruction;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UncheckedIOException;
-import org.junit.After;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+import java.util.Properties;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedConstruction;
 
-@RunWith(PowerMockRunner.class)
-public class OverridablePropertiesTests {
+class OverridablePropertiesTests {
 
     private static final String PROPERTY_SOURCE_NAME = "test";
     private static final String PROPERTY_IN_BOTH = "bothprop";
     private static final String PROPERTY_IN_SYSTEM_PROPS = "sysprop";
 
-    @After
-    public void resetLoggerFactory() {
+    @AfterEach
+    void resetLoggerFactory() {
         System.getProperties().remove(PROPERTY_SOURCE_NAME + "." + PROPERTY_IN_BOTH);
         System.getProperties().remove(PROPERTY_SOURCE_NAME + "." + PROPERTY_IN_SYSTEM_PROPS);
     }
 
     @Test
-    public void propertyNotInEither() throws IOException {
+    void propertyNotInEither() throws IOException {
         final String defaultValue = "sensible_default";
         OverridableProperties properties = new OverridableProperties(PROPERTY_SOURCE_NAME);
         assertEquals(defaultValue, properties.getProperty("notpresent", defaultValue));
     }
 
     @Test
-    public void propertyInFileNotInSystemProperties() throws IOException {
+    void propertyInFileNotInSystemProperties() throws IOException {
         OverridableProperties properties = new OverridableProperties(PROPERTY_SOURCE_NAME);
         assertEquals("file value", properties.getProperty("infile", "default"));
     }
 
     @Test
-    public void propertyNotInFileInSystemProperties() throws IOException {
+    void propertyNotInFileInSystemProperties() throws IOException {
         final String expectedValue = "system value";
         System.setProperty(PROPERTY_SOURCE_NAME + "." + PROPERTY_IN_SYSTEM_PROPS, expectedValue);
 
@@ -51,7 +51,7 @@ public class OverridablePropertiesTests {
     }
 
     @Test
-    public void propertyInBothFileAndSystemProperties() throws IOException {
+    void propertyInBothFileAndSystemProperties() throws IOException {
         final String expectedValue = "system value";
         System.setProperty(PROPERTY_SOURCE_NAME + "." + PROPERTY_IN_BOTH, expectedValue);
 
@@ -60,7 +60,7 @@ public class OverridablePropertiesTests {
     }
 
     @Test
-    public void noPropertyFile() throws IOException {
+    void noPropertyFile() throws IOException {
         OverridableProperties properties = new OverridableProperties("no-property-file");
 
         final String defaultValue = "sensible_default";
@@ -68,16 +68,16 @@ public class OverridablePropertiesTests {
     }
 
     @Test
-    @PrepareForTest(OverridableProperties.class)
-    public void ioExceptionAtPropertiesLoading() throws Exception {
+    void ioExceptionAtPropertiesLoading() {
         IOException ioException = new IOException();
-        whenNew(OverridableProperties.class).withArguments(anyString()).thenThrow(ioException);
-
-        try {
-            OverridableProperties.createUnchecked("somesource");
-            fail("UncheckedIOException was not thrown");
-        } catch (UncheckedIOException uncheckedIOException) {
-            assertThat(uncheckedIOException.getCause(), is(ioException));
+        try (MockedConstruction<Properties> ignored =
+                mockConstruction(
+                        Properties.class,
+                        (mock, context) -> doThrow(ioException).when(mock).load(any(InputStream.class)))) {
+            UncheckedIOException uncheckedIoException =
+                    assertThrows(
+                            UncheckedIOException.class, () -> OverridableProperties.createUnchecked("test"));
+            assertThat(uncheckedIoException.getCause(), is(ioException));
         }
     }
 }
